507b2cada1d4216361a5eb34d2ea309ba441f1f5
leohan <hanliqun@flashexpress>
2021-12-14 11:38:18 +0800

    测试重新编辑

---
+++ src/main/java/com/example/demo/util/TestRevert.java
@@ -0,0 +1,13 @@
+package com.example.demo.util;
+
+/**
+ * 功能描述：
+ *
+ * @Author: hanlq
+ * @Date: 2021/12/14 11:37
+ */
+public class TestRevert {
+    public Integer getVersion(){
+        return 1;
+    }
+}
